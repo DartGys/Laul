dcf5b1aac3213a9ab8eb6b450e9d99621e929f08
DartGys <boda20043@gmail.com>
2023-10-11 00:03:17 +0300

    done first migration

--- src/Laul.WebUI/Program.cs
+++ src/Laul.WebUI/Program.cs
@@ -1,7 +1,11 @@
+using Laul.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
