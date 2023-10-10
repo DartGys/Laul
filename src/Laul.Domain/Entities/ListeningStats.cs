9a2fd53c6514200d568d5644bca6a60b3db95217
DartGys <boda20043@gmail.com>
2023-10-10 13:14:22 +0300

    First entities

---
+++ src/Laul.Domain/Entities/ListeningStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laul.Domain.Entities
+{
+    public class ListeningStats
+    {
+        public int Id { get; set; }
+        public DateTime ListeningDate { get; set; }
+        public int UserId { get; set; }
+        public int EntityId { get; set; }
+        public string EntityType { get; set; }
+    }
+}
