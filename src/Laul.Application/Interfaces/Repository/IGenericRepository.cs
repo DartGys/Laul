54cb83a5a840858023185dbd41cabfe358785bf5
DartGys <boda20043@gmail.com>
2023-10-17 18:25:23 +0300

    interfaces added

---
+++ src/Laul.Application/Interfaces/Repository/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laul.Application.Interfaces.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GeyById(int id); 
+        Task<IEnumerable<T>> GetAllAsync(); 
+        Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
