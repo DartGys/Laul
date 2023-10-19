0c731aebcb0c49652081e3d7ef481fb98bd1080e
DartGys <boda20043@gmail.com>
2023-10-19 22:48:29 +0300

    migration created, DB updated

---
+++ src/Laul.Infrastructure/Migrations/20231019194722_SongAddProperty.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Laul.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SongAddProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Songs",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Storage",
+                table: "Songs",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Storage",
+                table: "Songs");
+        }
+    }
+}
