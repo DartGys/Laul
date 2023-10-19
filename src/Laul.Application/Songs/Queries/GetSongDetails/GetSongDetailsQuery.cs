b2904fda3b2d17124c14aff501bc6cf3fb226b84
DartGys <boda20043@gmail.com>
2023-10-19 14:46:05 +0300

    queries added to Song

---
+++ src/Laul.Application/Songs/Queries/GetSongDetails/GetSongDetailsQuery.cs
@@ -0,0 +1,14 @@
+﻿using Laul.Application.Common.Mapping;
+using Laul.Domain.Entities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Laul.Application.Songs.Queries.GetSongDetails
+{
+    public class SongDetails : IMapWith<Song>
+    {
+        public Guid ArtitId;
+        public string Title;
+        public int Duration;
+        public string Genre;
+    }
+}
