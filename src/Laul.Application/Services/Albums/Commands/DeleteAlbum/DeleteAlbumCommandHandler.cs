005f505cb8b3d5cf65bcb0d125e834eca18c5fcb
DartGys <boda20043@gmail.com>
2023-10-20 14:08:53 +0300

    album command add

---
+++ src/Laul.Application/Services/Albums/Commands/DeleteAlbum/DeleteAlbumCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laul.Application.Services.Albums.Commands.DeleteAlbum
+{
+    internal class DeleteAlbumCommandHandler
+    {
+    }
+}
--- src/Laul.Application/Services/Songs/Commands/CreateSong/CreateSongCommandHandler.cs
+++ src/Laul.Application/Services/Songs/Commands/CreateSong/CreateSongCommandHandler.cs
@@ -5,7 +5,7 @@ using Laul.Application.Interfaces.Persistance;
 
 namespace Laul.Application.Services.Songs.Commands.CreateSong
 {
-    public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, Guid>
+    public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, int>
     {
         private readonly IUnitOfWork _unitOfWork;
 
@@ -14,21 +14,21 @@ namespace Laul.Application.Services.Songs.Commands.CreateSong
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<Guid> Handle(CreateSongCommand request, CancellationToken cancellationToken)
+        public async Task<int> Handle(CreateSongCommand command, CancellationToken cancellationToken)
         {
             var song = new Song()
             {
-                ArtistId = request.ArtistId,
-                Title = request.Title,
-                Duration = request.Duration,
-                Genre = request.Genre,
-                AlbumId = request.AlbumId,
+                ArtistId = command.ArtistId,
+                Title = command.Title,
+                Duration = command.Duration,
+                Genre = command.Genre,
+                AlbumId = command.AlbumId,
             };
 
             await _unitOfWork.Song.AddAsync(song);
             await _unitOfWork.SaveChangeAsync();
 
-            return song.ArtistId;
+            return song.Id;
         }
     }
 }
