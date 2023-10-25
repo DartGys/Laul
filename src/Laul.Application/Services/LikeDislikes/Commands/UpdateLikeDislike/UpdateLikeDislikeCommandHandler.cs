ae2e1ea8a8e393b4dd99042c158a2b187ad95bb8
DartGys <boda20043@gmail.com>
2023-10-25 18:41:31 +0300

    LikeDislike command

---
+++ src/Laul.Application/Services/LikeDislikes/Commands/UpdateLikeDislike/UpdateLikeDislikeCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laul.Application.Services.LikeDislikes.Commands.UpdateLikeDislike
+{
+    internal class UpdateLikeDislikeCommandHandler
+    {
+    }
+}
